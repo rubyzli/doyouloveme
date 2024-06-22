dfe330003174b1fd1ee28a91489dae06899f1a00
Jessica <jr.simon07@gmail.com>
2024-06-22 22:03:00 +0200

    more changes

--- doesBabyLoveMe/vite.config.js
+++ doesBabyLoveMe/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Ensure that your JS files are correctly handled by Rollup
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  base: '/',
+});
